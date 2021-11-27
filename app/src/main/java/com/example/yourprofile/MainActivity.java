4800ad6eb68aee52d23080674eba742fad05f14f
Nazhier-Rijalana <nazhier9974@gmail.com>
2021-11-27 14:09:35 +0700

    inital project

---
+++ app/src/main/java/com/example/yourprofile/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.yourprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Profile.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/yourprofile/Profile.java
@@ -0,0 +1,26 @@
+package com.example.yourprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        getSupportActionBar().hide();
+        Button button = (Button) findViewById(R.id.button_back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+    }
+}
